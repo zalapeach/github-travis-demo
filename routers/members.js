f73f94021aea24078c010d405e3b0ae328f35e0e
Juan Carlos <zalapeach@gmail.com>
2020-05-13 14:37:38 -0500

    Added comment sections for the exercise

--- routers/members.js
+++ routers/members.js
@@ -13,4 +13,14 @@ router.get('/juan-carlos-aguilar-rincon', (req, res, next) => {
   });
 });
 
+//router.get('/your-full-name-route', (req, res, next) => { // Change by your full name
+  //res.render('members/show', {
+    //name: 'You full name',// Change by your full name
+    //org: 'Your organization',// Change by your full name
+    //github: 'zalapeach',// Change by your github user
+    //photo: '/images/members/your-image.png' //Change by your image
+    // add your image to public/members/
+  //});
+//});
+
 module.exports = router;
