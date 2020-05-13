e01d56485a244705cd0674685c506ca4a4ec9cf2
MonseSalasV <56927888+MonseSalasVi@users.noreply.github.com>
2020-05-13 16:49:49 -0500

    members
    new member

--- routers/members.js
+++ routers/members.js
@@ -13,6 +13,16 @@ router.get('/juan-carlos-aguilar-rincon', (req, res, next) => {
   });
 });
 
+router.get('monse-salas', (req, res, next) => { // Change by your full name
+  res.render('members/show', {
+    name: 'Monse Salas V',// Change by your full name
+    org: 'Laboratoria',// Change by your full name
+    github: 'MonseSalasVi',// Change by your github user
+    photo: '/images/members/your-image.png' //Change by your image
+    add your image to public/members/
+  });
+});
+
 //router.get('/your-full-name-route', (req, res, next) => { // Change by your full name
   //res.render('members/show', {
     //name: 'You full name',// Change by your full name
